c3404f9551d68f9dfe9d809990bb9774fc0535d6
arjan-do <arjan-doets@live.nl>
2013-01-10 14:42:21 +0100

    .gitignore effe checken

--- src/project2/java.java
+++ src/project2/java.java
@@ -14,6 +14,6 @@ public class java {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("We zijn Begonnen");
     }
 }
